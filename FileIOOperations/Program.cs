b7b9769898e8bfd7b3d0829f1f9bb20a6eb5634c
asmithasatesh <asmithasatesh@gmail.com>
2021-07-25 12:18:49 +0530

    [Asmitha]File IO Operations

--- FileIOOperations/Program.cs
+++ FileIOOperations/Program.cs
@@ -6,7 +6,42 @@ namespace FileIOOperations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Get option from User
+            Console.WriteLine("Welcome to File Operations!");
+            Console.WriteLine("Enter 1-For File Operations");
+            int options = Convert.ToInt32(Console.ReadLine());
+            switch(options)
+            {
+                case 1:
+                    //Get choice on which Io operations to Perform
+                    Console.WriteLine("Enter 1-To check Whether File Exist!");
+                    Console.WriteLine("Enter 2-To read all Lines of File");
+                    Console.WriteLine("Enter 3-To read All Text in File");
+                    Console.WriteLine("Enter 4-Create a Copy of another file");
+                    Console.WriteLine("Enter 5-Delete a File");
+                    int choice= Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            FileOperations.FileExists(@"D:\Assignments\FileIOOperations\FileIOOperations\File.txt");
+                            break;
+                        case 2:
+                            FileOperations.ReadAllLine();
+                            break;
+                        case 3:
+                            FileOperations.ReadAllText();
+                            break;
+                        case 4:
+                            FileOperations.FileCopy();
+                            break;
+                        case 5:
+                            FileOperations.DeleteFile();
+                            break;
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
